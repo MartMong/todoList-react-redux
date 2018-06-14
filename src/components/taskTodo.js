35c704e4ec9b6b064bfa5691b26bf22bf51cbd00
Mong <smartha55@hotmail.com>
2018-06-14 14:58:09 +0700

    edit block appear

--- src/components/taskTodo.js
+++ src/components/taskTodo.js
@@ -3,22 +3,28 @@ import {connect} from 'react-redux';
 import { Link } from 'react-router';
 
 class TaskTodo extends Component{
-    deleteList(id){
-        this.props.delete_todo(id);
+    constructor(props){
+        super(props);
+        this.state={
+            showEdit:false
+        }
+    }
+
+    editList(id){
+        console.log('in edit');
+        this.setState({showEdit:true});
     }
 
     renderList(){
         var { todoList } = this.props.todo;
-
-
-
         return todoList.map((msg, index) => {
-            
             return (
                 <li className="collection-item" key={index}  id = {index}  >
-                
                     {msg}
-                    <button onClick = {()=>{this.deleteList(index)}}  >
+                    <button onClick = {()=>{this.props.delete_todo(index)}}  >
+                        delete
+                    </button>
+                    <button onClick = {()=>{this.editList(index)}}  >
                         edit
                     </button>
 
@@ -27,16 +33,32 @@ class TaskTodo extends Component{
         });
     }
     render(){
-        
-        return(
-            <ul className = "collection">
-                {this.renderList()}
-            </ul>
-                
-            
-        );
+            return (
+            <div>
+                <ul className = "collection">
+                    {this.renderList()}
+                </ul>
+                {this.state.showEdit?<AddEditBlock/>:null}
+            </div>
+            );
+         
     }
+
+
+   
 }
+
+const AddEditBlock = () => {
+    return(
+        <form  > 
+            <label>edit todo detail</label>
+            <input id = "todoEdit" />
+        </form>
+
+    )
+}
+
+
 const mapStateToProps = (state)=>(
 
     {
